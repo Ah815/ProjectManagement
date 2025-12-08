0c1a4cc64551b0dbad144f22b846cdebd4b6611b
Ah815 <ahhhussain1234@gmail.com>
2025-12-08 13:18:00 +0500

    folder structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import dotenv, { configDotenv } from "dotenv";
-import express from "express";
+import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
 
-app.listen(PORT, () => {
-  console.log(` my server is listing in ${PORT}`);
+app.get ('/', (req, res) => {
+    res.send('Hello, World!');
 });
+app.listen (PORT ,( ) => {
+    console.log (`Server is running on port ${PORT}`);
+})
